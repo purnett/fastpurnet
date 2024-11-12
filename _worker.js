21f3ad878403d56e93b41acec017a1b5cfc7c93c
purnett <itspurnet@gmail.com>
2024-11-12 16:38:48 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -28,7 +28,7 @@ let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='
 
 // Preferred address API interface
 let ipUrlTxt = [
-	'https://raw.githubusercontent.com/amclubs/am-cf-tunnel/main/ipv4.txt',
+	'https://raw.githubusercontent.com/davudsedft/fastpurnet/refs/heads/main/ipv4.txt',
 	// 'https://raw.githubusercontent.com/amclubs/am-cf-tunnel/main/ipv6.txt'
 ];
 let ipUrlCsv = [
